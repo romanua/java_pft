23d65d06266015a3d0ac636ebc64cdf1005b1e54
Roman Kochubei <roman.kochubei@omnigon.com>
2016-03-04 02:36:27 +0200

    Merge first test for lesson 2

--- sandbox/src/test/java/DistanceTest.java
+++ sandbox/src/test/java/DistanceTest.java
@@ -1,7 +1,6 @@
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import java.awt.*;
-import java.awt.Point;
+
 
 /**
  * Created by roman on 02.03.2016.
@@ -12,6 +11,6 @@ public class DistanceTest {
     public void testDistance() {
         Point p1 = new Point(2, 3);
         Point p2 = new Point(5, 7);
-        assert p1.distance(p1,p2) == 24;
+        Assert.assertEquals(p1.distance(p1,p2), 5.0);
     }
 }
